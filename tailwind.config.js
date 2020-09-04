1e5ef1ed0062374be29115ac5e21c5c16a5a8fe9
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-09-04 16:01:43 +0200

    added tailwind confing for keyframe animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        pulse: {
+          '0%, 100%': {
+            opacity: 1
+          },
+          '50%': {
+            opacity: 0
+          }
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
