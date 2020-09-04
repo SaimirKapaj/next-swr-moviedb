d3c3c2f0845da6ab4258d76628e5a77a1fb19cc8
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-09-04 16:03:22 +0200

    added Nav in Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+import Icon from 'components/Icon/Icon';
+import Logo from './Logo';
+import Nav from './Nav/Nav';
+
 const Header = () => {
+  const [openNav, setOpenNav] = useState(false);
+
   return (
-    <div>
-      <h1 className="text-xl">Header</h1>
+    <div className="flex items-center justify-between px-4 md:px-8 py-4">
+      <Nav open={openNav} close={() => setOpenNav(false)} />
+      <div className="cursor-pointer" onClick={() => setOpenNav(true)}>
+        <Icon name="menu" className="w-6" />
+      </div>
+      <div className="text-center">
+        <Logo />
+      </div>
+      <div className="cursor-pointer">
+        <Icon name="search" className="w-6" />
+      </div>
     </div>
   );
 };
