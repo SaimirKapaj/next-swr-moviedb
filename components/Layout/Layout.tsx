7853ae45712aec91fbc5cfc91091c0b523090f39
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-10-28 13:12:39 +0100

    removed footer from the Layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 
 import Header from 'components/Header';
-import Footer from 'components/Footer';
 
 type Props = {
   children?: React.ReactNode;
@@ -17,13 +16,8 @@ const Layout = ({ children, title = 'Next Typescript Tailwind starter' }: Props)
       </Head>
       <div className="flex">
         <div className="flex flex-col min-h-screen w-full">
-          <header className="border-b border-gray-300">
-            <Header />
-          </header>
+          <Header />
           <main className="flex-1 p-4 md:p-8">{children}</main>
-          <footer className="p-4 md:p-8">
-            <Footer />
-          </footer>
         </div>
       </div>
     </>
