2415e069a73e0c461a0518873db655fa7759b182
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-02 16:27:34 +0100

    added SWRConfing to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
+import { SWRConfig } from 'swr';
+
+import { GenresProvider } from 'context/genres';
+
 import 'styles/tailwind.base.css';
 import 'styles/base.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const fetcher = (url: string) => {
+  const prefix = url.includes('?page') ? '&' : '?';
+
+  return fetch(
+    `${process.env.NEXT_PUBLIC_ENDPOINT}/${url}${prefix}api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+  ).then((res) => res.json());
+};
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <SWRConfig value={{ fetcher }}>
+      <GenresProvider>
+        <Component {...pageProps} />
+      </GenresProvider>
+    </SWRConfig>
+  );
+};
 
 export default MyApp;
