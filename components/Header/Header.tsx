b929290f43d3b135213b1fb04932cf5fdde352e3
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-02 16:22:04 +0100

    added Search component in the Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,13 @@
 import Icon from 'components/Icon/Icon';
 import Nav from './Nav/Nav';
+import Search from './Search/Search';
 
 const Header = () => {
   return (
     <header className="border-b border-gray-300">
       <div className="flex items-center justify-between px-4 md:px-8 py-6">
         <Nav />
-        <button className="cursor-pointer flex items-center p-2 px-3 border border-gray-300 bg-gray-100 rounded-full focus:outline-none sm:w-64 w-full">
-          <Icon name="search" className="w-5 mr-2" />
-          <span className="text-gray-600">Search</span>
-        </button>
+        <Search />
       </div>
     </header>
   );
