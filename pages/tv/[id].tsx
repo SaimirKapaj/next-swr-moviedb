0734e0517d67c2b81df63268538f08a6e060cd73
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-02 16:36:34 +0100

    created empty tv detail page

---
+++ pages/tv/[id].tsx
@@ -0,0 +1,16 @@
+import Layout from 'components/Layout';
+import { useRouter } from 'next/router';
+
+const TvDetailPage = () => {
+  const { query } = useRouter();
+
+  return (
+    <Layout>
+      <div className="flex items-center justify-between my-5">
+        <h1 className="text-xl">{query.id}</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default TvDetailPage;
