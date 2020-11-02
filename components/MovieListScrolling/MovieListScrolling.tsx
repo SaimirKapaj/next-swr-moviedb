7c42a11e73b237c8f0bfff63ab21afbe51f40c16
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-02 16:17:51 +0100

    created MovieListScrolling component

---
+++ components/MovieListScrolling/MovieListScrolling.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import MovieItem from 'components/MovieItem';
+import LoadingMoviePlaceholder from 'components/loading-placeholders/LoadingMoviePlaceholder';
+import Link from 'next/link';
+
+interface Genres {
+  id: number;
+  name: string;
+}
+
+interface Props {
+  category: 'movie' | 'tv';
+  data: any;
+  genres: Genres[];
+}
+
+const MovieListScrolling = ({ category, data, genres }: Props) => {
+  return (
+    <div className="overflow-hidden relative">
+      <div className="bg-gradient-to-r from-transparent opacity-75 to-white w-32 h-full absolute right-0 z-30" />
+      <div className="flex flex-no-wrap max-w-full overflow-x-scroll overflow-y-hidden pb-4 -mb-3">
+        {data?.results
+          ? data?.results?.map((movie) => (
+              <div key={movie.id} className="flex-none mr-6 w-3/4 sm:w-1/2 lg:w-1/4 xl:w-1/5">
+                <Link href={`/${category}/${movie.id}`}>
+                  <a>
+                    <MovieItem
+                      image={movie.backdrop_path}
+                      title={movie.name ?? movie.title}
+                      vote={movie.vote_average}
+                      genres={genres?.filter((genre) => movie.genre_ids.includes(genre.id))}
+                    />
+                  </a>
+                </Link>
+              </div>
+            ))
+          : [...Array(5)].map((_, index) => (
+              <div className="flex-none mr-6 w-3/4 sm:w-1/2 lg:w-1/4 xl:w-1/5">
+                <LoadingMoviePlaceholder delay={index} key={index} />
+              </div>
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieListScrolling;
