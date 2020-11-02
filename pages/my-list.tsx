b0493dff4b26dd6ce16066d2886460b9283a9452
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-02 16:30:19 +0100

    created my list page

--- pages/my-list.tsx
+++ pages/my-list.tsx
@@ -1,12 +1,10 @@
-import useSWR from 'swr';
-
 import Layout from 'components/Layout';
 
 const MyListPage = () => {
   return (
     <Layout>
       <div className="flex items-center justify-between my-5">
-        <h1 className="text-xl">My List</h1>
+        <h1 className="text-xl flex items-center">My List</h1>
       </div>
     </Layout>
   );
