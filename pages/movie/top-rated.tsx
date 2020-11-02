499eee28bf95e6d0d1e142fab58b8ce15356ccd2
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-02 16:34:40 +0100

    created top rated movie page

---
+++ pages/movie/top-rated.tsx
@@ -0,0 +1,14 @@
+import Layout from 'components/Layout';
+import InfiniteLoadingList from 'components/InfiniteLoadingList';
+import TitleWithBackButton from 'components/TitleWithBackButton';
+
+const MovieTopRatedPage = () => {
+  return (
+    <Layout>
+      <TitleWithBackButton title="Top Rated" />
+      <InfiniteLoadingList url="movie/top_rated" category="movie" />
+    </Layout>
+  );
+};
+
+export default MovieTopRatedPage;
