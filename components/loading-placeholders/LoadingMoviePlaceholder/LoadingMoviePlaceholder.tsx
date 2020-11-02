e7ef26a56c8512dedf79b92ac68492317e231654
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-02 16:14:45 +0100

    created LoadingMoviePlaceholder component

---
+++ components/loading-placeholders/LoadingMoviePlaceholder/LoadingMoviePlaceholder.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  delay: number;
+}
+
+const LoadingMoviePlaceholder = ({ delay }: Props) => (
+  <div className="animate-pulse rounded-lg" style={{ animationDelay: `${delay * 100}ms` }}>
+    <div className="w-full h-56 bg-gray-300 rounded-lg p-4 flex flex-col justify-end relative">
+      <div className="w-8 h-8 bg-gray-400 rounded-full absolute top-0 right-0 mr-4 mt-4" />
+      <div>
+        <div className="w-3/4 h-4 mt-2 bg-gray-400 rounded-lg" />
+        <div className="w-1/2 h-4 mt-2 bg-gray-400 rounded-lg" />
+      </div>
+    </div>
+  </div>
+);
+
+export default LoadingMoviePlaceholder;
