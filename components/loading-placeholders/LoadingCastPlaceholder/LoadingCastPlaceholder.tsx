78970f2dc28f9be8cf07ae5c5f6ffce55cd47fa6
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-04 21:23:18 +0100

    added margin LoadingCastPlaceholder

--- components/loading-placeholders/LoadingCastPlaceholder/LoadingCastPlaceholder.tsx
+++ components/loading-placeholders/LoadingCastPlaceholder/LoadingCastPlaceholder.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LoadingCastPlaceholder = () => {
   return (
-    <div className="animate-pulse">
-      <div className="bg-gray-300 h-4 w-24 mt-16 mb-6 rounded" />
+    <div className="animate-pulse mt-6">
+      <div className="bg-gray-300 h-4 w-16 mt-16 mb-6 rounded" />
       <div className="overflow-hidden relative w-full">
         <div className="flex flex-no-wrap max-w-full overflow-x-scroll overflow-y-hidden pb-12 -mb-3">
           {[...Array(9)].map((_, index) => (
