c5b7bc24117f45dca6b7e8ecab966933df4892c8
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-04 15:28:33 +0100

    added BackButton to TitleWithBackButton

--- components/TitleWithBackButton/TitleWithBackButton.tsx
+++ components/TitleWithBackButton/TitleWithBackButton.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 
 import Icon from 'components/Icon';
+import ButtonBack from '../ButtonBack/ButtonBack';
 
 interface Props {
   title: string;
@@ -12,10 +13,8 @@ const TitleWithBackButton = ({ title }: Props) => {
   return (
     <div className="flex items-center justify-between my-5">
       <h1 className="text-xl flex items-center">
-        <button className="focus:outline-none mr-3 hover:text-teal-600" onClick={() => router.back()}>
-          <Icon name="arrow-left" className="w-5 h-5" />
-        </button>
-        {title}
+        <ButtonBack />
+        <span className="ml-3">{title}</span>
       </h1>
     </div>
   );
