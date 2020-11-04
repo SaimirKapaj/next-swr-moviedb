de2f315d6bcf6fac527609b1f75027f29c3ff9bf
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-11-04 15:43:28 +0100

    updated style in LoadingCastPlaceholder

--- components/loading-placeholders/LoadingCastPlaceholder/LoadingCastPlaceholder.tsx
+++ components/loading-placeholders/LoadingCastPlaceholder/LoadingCastPlaceholder.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingCastPlaceholder = () => {
   return (
     <div className="animate-pulse">
-      <div className="bg-gray-300 h-4 w-32 mt-16 mb-4 rounded" />
+      <div className="bg-gray-300 h-4 w-24 mt-16 mb-6 rounded" />
       <div className="overflow-hidden relative w-full">
         <div className="flex flex-no-wrap max-w-full overflow-x-scroll overflow-y-hidden pb-12 -mb-3">
           {[...Array(9)].map((_, index) => (
