40fc990e0a9846b16c7eba5ced1ddc15348c63d9
Saimir Kapaj <saimir.kapaj@pm-international.com>
2021-02-19 15:10:44 +0100

    add media type in movie detail page

--- pages/tv/[id].tsx
+++ pages/tv/[id].tsx
@@ -8,9 +8,14 @@ const TvDetailsPage = () => {
   const { query } = useRouter();
   const { details } = useDetails('tv', query.id);
 
+  const tvDatails = {
+    media_type: 'tv',
+    ...details
+  };
+
   return (
     <Layout>
-      <DetailsPage details={details} />
+      <DetailsPage details={tvDatails} />
     </Layout>
   );
 };
