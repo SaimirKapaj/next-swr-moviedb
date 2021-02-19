9068b7b2c561faa987bb1c656f914827d5fc5bb8
Saimir Kapaj <saimir.kapaj@pm-international.com>
2021-02-19 12:02:07 +0100

    changed back button style

--- components/ButtonBack/ButtonBack.tsx
+++ components/ButtonBack/ButtonBack.tsx
@@ -6,10 +6,7 @@ const ButtonBack = () => {
   const router = useRouter();
 
   return (
-    <button
-      className="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-full p-3 focus:outline-none"
-      onClick={() => router.back()}
-    >
+    <button className="bg-gray-800 rounded-full p-3 focus:outline-none" onClick={() => router.back()}>
       <Icon name="arrow-left" className="w-4 h-4" />
     </button>
   );
