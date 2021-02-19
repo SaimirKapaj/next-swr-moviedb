f0845243cef83928d4d909899549d9577d332e52
Saimir Kapaj <saimir.kapaj@pm-international.com>
2021-02-19 12:02:36 +0100

    changed Header styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Search from 'components/SearchBar';
 
 const Header = () => {
   return (
-    <header className="sm:border-b sm:border-gray-300">
+    <header className="sm:border-b sm:border-gray-800">
       <div className="flex items-center justify-between px-4 md:px-8 py-4 sm:py-6">
         <Nav />
         <Search />
