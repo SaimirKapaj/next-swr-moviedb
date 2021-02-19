5996ab05764076a5ff1d03b728a0505888f1b49c
Saimir Kapaj <saimir.kapaj@pm-international.com>
2021-02-19 12:05:13 +0100

    changed search bar color

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -13,7 +13,7 @@ const SearchModal = ({ children, onClose }: SearchModalProps) => {
   return (
     <div className="fixed w-screen h-screen flex justify-center left-0 top-0 z-50 overscroll-auto">
       <div className="absolute w-screen h-screen bg-black opacity-75 left-0 top-0 z-30" onClick={onClose}></div>
-      <div className="absolute md:rounded-lg p-4 md:max-w-screen-sm w-full md:h-auto h-screen bg-white transform md:translate-y-16 z-50 ">
+      <div className="absolute md:rounded-lg p-4 md:max-w-screen-sm w-full md:h-auto h-screen bg-gray-900 transform md:translate-y-16 z-50 ">
         {children}
       </div>
     </div>
@@ -63,10 +63,10 @@ const SearchBar = () => {
     <>
       <button
         onClick={() => setIsModalOpen(true)}
-        className="relative cursor-pointer flex items-center p-3 sm:p-2 px-4 sm:px-3  bg-gray-100 border border-gray-300 rounded focus:outline-none sm:w-48 w-full"
+        className="relative cursor-pointer flex items-center p-3 sm:p-2 px-4 sm:px-3  bg-gray-800 rounded focus:outline-none sm:w-48 w-full"
       >
         <Icon name="search" className="w-6 sm:w-5 sm:ml-0 mr-2 -ml-px" />
-        <span className="text-gray-600 text-lg">Search</span>
+        <span className="text-lg">Search</span>
       </button>
       {isModalOpen ? (
         <SearchModal onClose={() => setIsModalOpen(false)}>
@@ -77,23 +77,20 @@ const SearchBar = () => {
                   value={term}
                   onChange={(e) => setTerm(e.target.value)}
                   autoFocus
-                  className="p-3 pl-12 text-lg border border-teal-500 focus:outline-none w-full rounded"
+                  className="p-3 pl-12 text-lg bg-gray-800 focus:outline-none w-full rounded"
                 />
                 <Icon name="search" className="w-6 absolute ml-4" />
                 {term ? (
                   <span
                     onClick={() => setTerm('')}
-                    className="absolute cursor-pointer right-0 mr-3 text-white bg-teal-500 hover:bg-teal-600 p-1 rounded-full focus:outline-none"
+                    className="absolute cursor-pointer right-0 mr-3 text-white bg-blue-500 hover:bg-blue-600 p-1 rounded-full focus:outline-none"
                   >
                     <Icon name="cross" className="w-4" />
                   </span>
                 ) : null}
               </label>
             </form>
-            <button
-              onClick={() => setIsModalOpen(false)}
-              className="ml-3 rounded-full text-teal-600 md:hidden focus:outline-none"
-            >
+            <button onClick={() => setIsModalOpen(false)} className="ml-3 rounded-full md:hidden focus:outline-none">
               Close
             </button>
           </div>
@@ -105,13 +102,13 @@ const SearchBar = () => {
                   <label key={index} className="flex items-center relative mr-2 mt-2">
                     <button
                       onClick={() => redirect(term)}
-                      className="flex items-center justify-between pl-3 py-2 pr-6  text-sm bg-gray-100 border border-gray-300 rounded focus:outline-none"
+                      className="flex items-center justify-between pl-3 py-2 pr-6  text-sm bg-gray-800 rounded focus:outline-none"
                     >
                       <span className="mr-2">{term}</span>
                     </button>
                     <button
                       onClick={() => deleteSearchHistoryItem(index)}
-                      className="absolute right-0 mr-2 cursor-pointer bg-gray-100 hover:bg-gray-300 p-1 rounded-full focus:outline-none"
+                      className="absolute right-0 mr-2 cursor-pointer bg-gray-700 p-1 rounded-full focus:outline-none"
                     >
                       <Icon name="cross" className="w-3" />
                     </button>
