9ed3b94240248365ceb4ee0cb574bb5236d3ff36
Saimir Kapaj <saimir.kapaj@pm-international.com>
2021-02-21 21:54:39 +0100

    fixed pointer events issue in movie list scrolling

--- components/MovieListScrolling/MovieListScrolling.tsx
+++ components/MovieListScrolling/MovieListScrolling.tsx
@@ -11,8 +11,8 @@ interface Props extends GenresList {
 const MovieListScrolling = ({ mediaType, data, genres }: Props) => {
   return (
     <div className="overflow-hidden relative">
-      <div className="bg-gradient-to-r from-transparent opacity-75 to-black w-32 h-full absolute right-0 z-30" />
       <div className="flex flex-no-wrap max-w-full overflow-x-scroll overflow-y-hidden pb-4 -mb-3">
+        <div className="bg-gradient-to-r from-transparent opacity-75 to-black w-32 h-full absolute right-0 z-30 -m-1 pointer-events-none" />
         {data?.results
           ? data?.results?.map((movie) => (
               <div key={movie.id} className="flex-none mr-6 w-3/4 sm:w-1/2 lg:w-1/4 xl:w-1/5">
